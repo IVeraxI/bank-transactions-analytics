7f04599f3e2bf599619196ed969cab44e3bca0a5
Dazhy <homuskudaze@gmail.com>
2026-08-05 13:00:38 +0700

    feat: add get_greeting and read_operations_excel functions

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
+import pandas as pd
+
 
 def get_greeting(current_time: str) -> str:
     """Принимает строку вида '2021-12-21 18:30:15' и возвращает приветствие."""
     time_obj = datetime.strptime(current_time, "%Y-%m-%d %H:%M:%S")
-
     hour = time_obj.hour
 
     if 6 <= hour < 12:
@@ -17,3 +18,10 @@ def get_greeting(current_time: str) -> str:
         greeting = "Доброй ночи"
 
     return greeting
+
+
+def read_operations_excel(file_path: str) -> list[dict]:
+    """Читает Excel-файл с транзакциями и возвращает список словарей."""
+    df = pd.read_excel(file_path)
+    operations = df.to_dict(orient="records")
+    return operations
